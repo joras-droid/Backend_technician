ba0edb567464c87033d8dae0147a26fdcedb26b5
Prashant Bhandari <59383835+prashant0ngx@users.noreply.github.com>
2026-03-06 14:06:14 +0545

    Remove dist and logs from repo and add gitignore

--- dist/src/common/services/email.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class EmailService {
-    private configService;
-    private transporter;
-    constructor(configService: ConfigService);
-    sendPasswordResetOtp(to: string, otp: string, firstName?: string): Promise<void>;
-    private getPasswordResetOtpTemplate;
-}
--- prisma/migrations/20260209052639_add_before_after_work_photos/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "WorkOrder" ADD COLUMN     "afterWorkPhotos" TEXT[] DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN     "beforeWorkPhotos" TEXT[] DEFAULT ARRAY[]::TEXT[];
